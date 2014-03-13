120f5c3f4129aaef1939cd10f7691db8429d1eab
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-03-13 11:41:43 +0100

    Setup project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,24 @@
 (defproject gen-java-src "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Generate Java code in the browser"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.6.0-RC1"]
+                 [org.clojure/clojurescript "0.0-2173"]
+                 [com.cemerick/double-check "0.5.6-SNAPSHOT"]]
+  :plugins [[lein-cljsbuild "1.0.2"]]
+  :cljsbuild {:builds
+              {:development
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "out/dev/gen-java.js"
+                           :output-dir "out/dev"
+                           :optimizations :whitespace
+                           :pretty-print true
+                           :libs [""]}}
+               :production
+               {:source-paths ["src/cljs"]
+                :compiler {:output-to "out/production/gen-java.js"
+                           :output-dir "production"
+                           :optimizations :advanced
+                           :pretty-print false
+                           :libs [""]}}}})
