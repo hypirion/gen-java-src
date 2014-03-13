ac95efcc625ee4364b5bde7a5d502b571496acfd
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-03-13 11:36:28 +0100

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to gen-java-src
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/gen_java_src/core_test.clj
@@ -0,0 +1,7 @@
+(ns gen-java-src.core-test
+  (:require [clojure.test :refer :all]
+            [gen-java-src.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
