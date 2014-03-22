d133edee0a6bdb32ae3bf1a12f15c536d96015a6
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-03-22 15:58:04 +0100

    Update html-file and main cljs ns.

--- src/cljs/gen_java_src/main.cljs
+++ src/cljs/gen_java_src/main.cljs
@@ -4,14 +4,25 @@
             [simple-check.properties :as prop]
             [gen-java-src.generators :as jgen]
             [gen-java-src.pprint :as pp]
+            [gen-java-src.reductions :refer [reduce-class]]
             [domina.xpath :refer [xpath]]
-            [domina])
+            [domina :as d]
+            [clojure.browser.event :as event])
   (:require-macros [simple-check.properties :as prop]))
 
-(def sort-idempotent-prop
-  (prop/for-all [v (gen/vector gen/int)]
-    (= (sort v) (sort (sort v)))))
+(defn htmlized-class
+  "Could probably be optimised."
+  [n]
+  (let [r (nth (gen/sample-seq jgen/class-gen) n)]
+    (pp/htmlify (reduce-class r))))
 
-(doseq [s (gen/sample jgen/class-gen)]
-  (domina/append! (xpath "//body")
-                  (pp/htmlify s)))
+#_#_#_
+(def codegen-button (d/by-id "btn"))
+(def code-div (d/by-id "code-div"))
+
+(event/listen codegen-button "click"
+              (fn [] (let [level (-> (d/by-id "level") d/value str js/parseFloat
+                                    Math/floor)]
+                      (d/destroy-children! code-div)
+                      (d/append! code-div (htmlized-class level)))))
+
